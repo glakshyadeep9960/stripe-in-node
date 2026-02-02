d2e2adb40e8f401207bd95bbe8053985913355d2
lakshyadeepgautam <lakshyadeepgautam@gmail.com>
2026-02-02 18:15:43 +0530

    changes

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function databaseConnection(req, res) {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to Database");
   } catch (error) {
     console.log("Error in Connection");
