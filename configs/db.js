0fa7a87e69caf0a2ec811d425ce6bf7e531b84ce
lakshyadeepgautam <lakshyadeepgautam@gmail.com>
2026-02-02 20:59:32 +0530

    change flow

--- configs/db.js
+++ configs/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function databaseConnection(req, res) {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to Database");
+    console.log("Connected to Mongo DB");
   } catch (error) {
     console.log("Error in Connection");
   }
